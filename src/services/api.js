1d6565d244225f1922b5a4b16dd7033b57ab096d
BAlshowaikh <batoolalshowaikh04@outlook.com>
2026-02-02 20:51:10 +0300

    Wrapping the auth around the app component and mofify the axios

--- src/services/api.js
+++ src/services/api.js
@@ -11,4 +11,16 @@ const api = axios.create({
     }
 })
 
+// For each request attach the access token so the BE validate it
+api.interceptors.request.use((config) => {
+        const token = localStorage.getItem("access_token")
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`
+        }
+        return config
+    },(error) => {
+        return Promise.reject(error);
+    }
+)
+
 export default api
